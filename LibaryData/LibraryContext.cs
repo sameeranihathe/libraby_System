f2b8f8c18d0ef2a83b6f50df5542d245f6d0bfb7
sameeranihathe <sameeranihathe@gmail.com>
2018-03-09 22:36:01 +0530

    database migrated/Patron table created

---
+++ LibaryData/LibraryContext.cs
@@ -0,0 +1,13 @@
+﻿using LibaryData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace LibaryData
+{
+    public class LibraryContext: DbContext
+    {
+         public LibraryContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Patron> Patrons { get; set; }
+    }
+}
