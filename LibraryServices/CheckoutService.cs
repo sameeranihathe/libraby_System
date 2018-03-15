acc1cb89162a0bdf285f6d314174acc2286bc4fd
sameeranihathe <sameeranihathe@gmail.com>
2018-03-15 14:17:59 +0530

    libraryservice module

---
+++ LibraryServices/CheckoutService.cs
@@ -0,0 +1,87 @@
+﻿using LibaryData;
+using LibaryData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryServices
+{
+    public class CheckoutService : ICheckout
+    {
+        private LibraryContext _context;
+
+        public CheckoutService(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Checkout newCheckout)
+        {
+            _context.Add(newCheckout);
+            _context.SaveChanges();
+        }
+
+        public void CheckInItem(int assetId, int libraryCardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckOutItem(int assetId, int libraryCardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Checkout> GetAll()
+        {
+            return _context.Checkouts;
+        }
+
+        public Checkout GetById(int checkoutId)
+        {
+            return GetAll()
+                .FirstOrDefault(Checkout => Checkout.Id == checkoutId);
+        }
+
+        public IEnumerable<CheckoutHistory> GetCheckoutHistory(int id)
+        {
+            return _context.CheckoutHistories
+                .Include(h=>h.libraryAsset)
+                .Include(h=>h.LibraryCard)
+                .Where(h => h.libraryAsset.Id == id);
+        }
+
+        public string GetCurrentHoldPatronName(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetCurrentHoldPlaced(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Hold> GetCurrentHolds(int id)
+        {
+            return _context.Holds
+                .Include(h => h.LibraryAsset)
+                .Where(h => h.LibraryAsset.Id ==id);
+        }
+
+        public void MarkFound(int assetId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MarkLost(int assetId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlaceHold(int assetId, int libraryCardId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Library_Management_System/Startup.cs
+++ Library_Management_System/Startup.cs
@@ -27,6 +27,7 @@ namespace Library_Management_System
             services.AddMvc();
             services.AddSingleton(Configuration);
             services.AddScoped<ILibraryAsset, LibraryAssetService>();
+            services.AddScoped<ICheckout, CheckoutService>();
 
             services.AddDbContext<LibraryContext>(options
                 =>options.UseSqlServer(Configuration.GetConnectionString("LibraryConnection")));
