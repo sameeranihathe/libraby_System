7fcbba2a909239060ee845235cd7e494dbac144e
sameeranihathe <sameeranihathe@gmail.com>
2018-03-20 08:14:38 +0530

    home page modifications

--- Library_Management_System/Controllers/HomeController.cs
+++ Library_Management_System/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Library_Management_System.Models;
 
@@ -15,20 +11,7 @@ namespace Library_Management_System.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
+        
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
