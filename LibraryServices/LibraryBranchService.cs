02f0be8160dfb1ade9068c94d5a7a8a4f925736c
sameeranihathe <sameeranihathe@gmail.com>
2018-03-21 21:28:57 +0530

    branch controller created

---
+++ LibraryServices/LibraryBranchService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryServices
+{
+    public class LibraryBranchService : ILibraryBranch
+    {
+    }
+}
---
+++ Library_Management_System/Controllers/BranchController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibaryData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management_System.Controllers
+{
+    public class BranchController : Controller
+    {
+        private ILibraryBranch _branch;
+
+        public BranchController(ILibraryBranch branch)
+        {
+            _branch = branch;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
