eecd8d9931190434e48f4c9be9e43cc0cc56ca8d
sameeranihathe <sameeranihathe@gmail.com>
2018-03-22 15:15:45 +0530

    ILibraryBranch interface created

--- LibaryData/ILibraryBranch.cs
+++ LibaryData/ILibraryBranch.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using LibaryData.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace LibaryData
 {
-    public class ILibraryBranch
+    public interface ILibraryBranch
     {
-       
+        IEnumerable<ILibraryBranch> GetAll();
+        IEnumerable<Patron> GetPatron(int branchId);
+        IEnumerable<LibraryAsset> GetAssets(int branchId);
+        IEnumerable<string> GetBranchHours(int branchId);
+        LibraryBranch Get(int branchId);
+        void Add(LibraryBranch newBranch);
+        void IsBranchOpen(int branchId);
     }
 }
